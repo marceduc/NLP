32267fe71b096061fa35085753b66ed6fe410090
Jeremy <jer15@web.de>
2018-11-20 16:14:13 +0100

    aktueller Stand: Indziert Informationen mit Hilfe von index_parser.java

--- 2_Volltextsuche/Uebung2-skeleton/service/PubMedSearchEngineImpl.java
+++ 2_Volltextsuche/Uebung2-skeleton/service/PubMedSearchEngineImpl.java
@@ -1,23 +1,38 @@
 package service;
 
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public class PubMedSearchEngineImpl implements PubMedSearchEngine {
-
+	
+	public static HashMap<String, String> titleWL = new HashMap<String, String>();
+	public static HashMap<String, String> abstractWL = new HashMap<String, String>();
+	public static HashMap<String, String> yearL = new HashMap<String, String>();
+	public static HashMap<String, String> meshL = new HashMap<String, String>();
+	
     public PubMedSearchEngineImpl() {
         //Don't change the constructor!
     }
 
     @Override
     public void buildIndex(Path pubmedDirectory) {
-        //TODO: Implement creation of indices!
+      
+    	//Indiziert alle Informationen
+    	index_parser ip = new index_parser(pubmedDirectory);
+    	ip.run();
+    	titleWL = index_parser.titleWL;
+    	abstractWL = index_parser.abstractWL;
+    	yearL = index_parser.yearL;
+    	meshL = index_parser.meshL;
+    	
     }
 
     @Override
     public Set<Integer> query(String query) {
-        // TODO: Implement the query logic!
+        
+    	
         return new HashSet<>();
     }
 }

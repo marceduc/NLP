93cc1106194beb4d0f02ee356d1e3878acfefabe
marc <scheumar@hu-berlin.de>
2019-01-20 18:09:56 +0100

    exercises 5 done, evaluation script not run yet

---
+++ 5_NER/uebung5-NotWorkingAsIntended.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import sys
+
+def main():
+    INPUT_FILE = sys.argv[1]
+    OUTPUT_FILE = sys.argv[2]
+    STOP_WORDS_FILE = 'english_stop_words.txt'
+    DICT_FILE = 'genenames_extended.txt'
+
+    #load gene dict, and stopwords
+    stopwords = pd.read_csv(STOP_WORDS_FILE, sep = '\t', names= ['stopword'])
+    genenames = pd.read_csv(DICT_FILE, sep = '\t', names= ['GenName'])
+
+    #create set of lower case entries
+    genes = set(genenames.GenName.str.lower().unique())
+    stopwords = set(stopwords.stopword.str.lower().unique())
+
+    # load classification data
+    test = pd.read_csv(INPUT_FILE, sep = '\t',header=None, names= ['token', 'label'])
+    test['lower'] = test.token.str.lower()
+
+    # make predictions
+    preds = []
+
+    for i in range(len(test)):
+        if test.lower.iloc[i] in stopwords:
+            preds.append('O')
+        elif test.lower.iloc[i] in genes:
+            preds.append('B-protein')
+        else:
+            preds.append('O')
+
+    pred_df = pd.DataFrame({'token':test.token.values, 'preds':preds})
+    pred_df.to_csv(OUTPUT_FILE, sep = '\t', header = None, index=None)
+
+main()
